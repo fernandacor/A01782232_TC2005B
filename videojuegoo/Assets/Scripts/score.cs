b53a6eced2d3480a152702db6cec58b07ec9da48
Fernanda Cantú <fercantu56@gmail.com>
2023-03-01 12:30:49 -0600

    script para ir sumando el score

---
+++ videojuegoo/Assets/Scripts/score.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score : MonoBehaviour
+{
+    int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddPoints(int amount)
+    {
+        score += amount;
+        Debug.Log("New Score: " + score);
+    }
+}
