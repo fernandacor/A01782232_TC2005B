4d58ee5154ea8f35099d66c32c8e625d1b53ded9
Fernanda Cantú <fercantu56@gmail.com>
2023-03-06 18:56:19 -0600

    script para movimiento

--- crossyRoad_dupe/Assets/Scripts/movimientoTopDown.cs
+++ crossyRoad_dupe/Assets/Scripts/movimientoTopDown.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class movimientoTopDown : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private Vector1 direccion;
+
+    private RigidBody2D rb2D;
 
     // Update is called once per frame
+    void Start()
+    {
+        rb2D = GetComponent<RigidBody2D>();
+    }
+
     void Update()
     {
-        
+        direccion = new Vector1(Input.GetAxisRaw("vertical")).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb2D.MovePosition(rb2D.position + direccion * velocidadMovimiento * Time.fixedDeltaTime);
     }
 }
